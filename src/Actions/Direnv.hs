619696f4d296bc383c874e1f89b038c1374280a3
Daniel Harvey <danieljamesharvey@gmail.com>
2020-06-03 14:46:29 +0100

    Build and release script

--- src/Actions/Direnv.hs
+++ src/Actions/Direnv.hs
@@ -1,4 +1,4 @@
-module Actions.Direnv (createDirenvRc) where
+module Actions.Direnv (createDirenvRc, direnvReload) where
 
 -- here we create an .envrc which adds our new apps to the $PATH
 type Path = String
@@ -7,3 +7,13 @@ createDirenvRc :: Path -> IO ()
 createDirenvRc path = do
   writeFile path "PATH_add $(nix-mate paths)"
   pure ()
+
+-- run a shell action that throws
+safeShell :: String -> IO ()
+safeShell command = do
+  let myShell = (shell command) {cwd = (Just ".")}
+  _ <- try (readCreateProcess (myShell) "")
+  pure ()
+
+direnvReload :: IO ()
+direnvReload = safeShell "direnv reload"
